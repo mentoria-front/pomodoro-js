ae9873fbbe636c91d53a50c688045a4344855c8f
marcos012 <marcospaulo.ad@gmail.com>
2022-12-15 16:40:58 -0300

    Created simple pomodoro

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const FIVE_MINUTES = 5 * 60;
+const LONG_MINUTES = 10 * 60;
+const ONE_SECOND = 1000;
+
+let timeout;
 
 function App() {
+
+  const [time, setTime] = useState(FIVE_MINUTES);
+  const [isActive, setIsActive] = useState(true);
+
+  function handleChangeTimer(timetoSet) {
+    stopCounter()
+    setTime(timetoSet)
+    setIsActive(true);
+  }
+
+  function stopCounter() {
+    clearTimeout(timeout)
+    setIsActive(false);
+  }
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  useEffect(() => {
+    if (isActive && time > 0) {
+      timeout = setTimeout(() => {
+        setTime(time - 1);
+      }, ONE_SECOND);
+    } else {
+      setIsActive(false)
+    }
+
+  }, [time, isActive]);
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button onClick={() => handleChangeTimer(FIVE_MINUTES)}>Short breake</button>
+        <button onClick={() => handleChangeTimer(LONG_MINUTES)}>Long breake</button>
+
+        <p>{minutes} : {seconds}</p>
+       
       </header>
     </div>
   );
