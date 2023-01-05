edf009216a4b154f3e3a1c79078262cdf909ecfd
marcos012 <marcospaulo.ad@gmail.com>
2023-01-05 16:48:51 -0300

    created components for Timer and Quote

--- src/App.js
+++ src/App.js
@@ -1,56 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
-
-const FIVE_MINUTES = 5 * 60;
-const LONG_MINUTES = 10 * 60;
-const ONE_SECOND = 1000;
-
-let timeout;
+import { Pomodoro } from './pages/Pomodoro';
 
 function App() {
-
-  const [time, setTime] = useState(FIVE_MINUTES);
-  const [isActive, setIsActive] = useState(true);
-
-  function handleChangeTimer(timetoSet) {
-    stopCounter()
-    setTime(timetoSet)
-    setIsActive(true);
-  }
-
-  function stopCounter() {
-    clearTimeout(timeout)
-    setIsActive(false);
-  }
-
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60;
-
-  useEffect(() => {
-    if (isActive && time > 0) {
-      timeout = setTimeout(() => {
-        setTime(time - 1);
-      }, ONE_SECOND);
-    } else {
-      setIsActive(false)
-    }
-
-  }, [time, isActive]);
-
-
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <button onClick={() => handleChangeTimer(FIVE_MINUTES)}>Short breake</button>
-        <button onClick={() => handleChangeTimer(LONG_MINUTES)}>Long breake</button>
-
-        <p>{minutes} : {seconds}</p>
-       
-      </header>
+      <Pomodoro />
     </div>
   );
 }
---
+++ src/components/Quote.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import { useQuery } from 'react-query'
+import { fetchQuotes } from '../services/getQuotes';
+
+const FIVE_MINUTES = 5 * 60;
+
+export default function Quote({ currentTime }) {
+  const [quotes, setQuotes] = useState([])
+  const [quote, setQuote] = useState();
+
+  const { data } = useQuery('quotes', fetchQuotes, {
+    staleTime: FIVE_MINUTES,
+  });
+
+  useEffect(() => {
+    setQuotes(data);
+  }, [data])
+
+
+  useEffect(() => {
+    if (currentTime === 0) {
+      getRandomQuote();
+    }
+  }, [currentTime])
+
+  function getRandomQuote() {
+    const randomNumber = Math.floor(Math.random() * quotes.length)
+    const randomQuote =  quotes[randomNumber];
+
+    setQuote(randomQuote)
+  }
+
+  return (
+    <div>
+      {quote && (
+          <>
+            <p>{quote.text}</p>
+            <p>{quote.author}</p>
+          </>
+        )}
+    </div>
+  )
+}
